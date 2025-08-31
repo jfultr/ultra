e675f427bd1659253fb66de2ec2e510c377d3962
Yakubov Farkhat <farhatyakubov.com>
2025-09-01 00:10:08 +0500

    env.example + some correction with envs + added egg-info

--- fastapi-app/app/api/deps.py
+++ fastapi-app/app/api/deps.py
@@ -18,7 +18,7 @@ def get_db() -> Generator[Session, None, None]:
         db.close()
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{settings.API_V1_STR}/auth/login")
 
 
 def get_current_user(
--- fastapi-app/app/main.py
+++ fastapi-app/app/main.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI
 
 from app.api.routers import auth, items
+from app.core.config import settings
 from app.db.base import Base
 from app.db.session import engine
 
 
 def create_app() -> FastAPI:
-    application = FastAPI(title="FastAPI App", version="0.1.0")
+    application = FastAPI(title=settings.PROJECT_NAME, version="0.1.0", debug=settings.DEBUG)
 
     # Routers
-    application.include_router(auth.router, prefix="/api")
-    application.include_router(items.router, prefix="/api")
+    application.include_router(auth.router, prefix=settings.API_V1_STR)
+    application.include_router(items.router, prefix=settings.API_V1_STR)
 
     @application.on_event("startup")
     def on_startup() -> None:
